6d00ffbf2c2d2c0e0275d62bc15a8a1327368286
Kushagra Anand <kushrokz95@gmail.com>
2020-05-19 11:32:54 +0530

    Updates

---
+++ precache-manifest.217e89e4c94b4c2f7ab41096768f5427.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f12c23703914910eb58ed335a7e30c3",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "814e45a34c6ba85212fa",
+    "url": "/react-to-do-list/static/css/main.031e540d.chunk.css"
+  },
+  {
+    "revision": "86020ece81e1cb16eaba",
+    "url": "/react-to-do-list/static/js/2.e132c9e9.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/react-to-do-list/static/js/2.e132c9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814e45a34c6ba85212fa",
+    "url": "/react-to-do-list/static/js/main.aba353b2.chunk.js"
+  },
+  {
+    "revision": "bb15b691843d5261dabd",
+    "url": "/react-to-do-list/static/js/runtime-main.32d71c53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
